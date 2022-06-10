9905115946a2e338bdcdb5110b9d556db2b0ee4d
njtierney <nicholas.tierney@gmail.com>
2022-06-10 17:51:35 +0800

    add internal functions required for building new distributions

---
+++ R/internals.R
@@ -0,0 +1,18 @@
+# need some internal greta functions accessible
+distrib <- greta::.internals$nodes$constructors$distrib
+check_dims <- greta::.internals$checks$check_dims
+check_in_family <- greta::.internals$checks$check_in_family
+check_positive <- greta::.internals$checks$check_positive
+check_unit <- greta::.internals$checks$check_unit
+has_representation <- greta::.internals$greta_arrays$has_representation
+representation <- greta::.internals$greta_arrays$representation
+tf_lchoose <- greta::.internals$tensors$tf_lchoose
+tf_lbeta <- greta::.internals$tensors$tf_lbeta
+distribution_node <- greta::.internals$nodes$node_classes$distribution_node
+as.greta_array <- greta::.internals$greta_arrays$as.greta_array
+tf_iprobit <- greta::.internals$tensors$tf_iprobit
+fl <- greta::.internals$utils$misc$fl
+tf_as_float <- greta::.internals$tensors$tf_as_float
+tf_rowsums <- greta:::tf_rowsums
+op <- greta::.internals$nodes$constructors$op
+tf_as_float <- greta::.internals$tensors$tf_as_floa
