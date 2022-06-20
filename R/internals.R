fcfad31dec486beb3a5ff314144a6d0ee093d21c
njtierney <nicholas.tierney@gmail.com>
2022-06-20 14:39:20 +0800

    re-group the internals

--- R/internals.R
+++ R/internals.R
@@ -1,15 +1,24 @@
 # need some internal greta functions accessible
-distrib <- .internals$nodes$constructors$distrib
+
 check_dims <- .internals$checks$check_dims
 check_in_family <- .internals$checks$check_in_family
 check_positive <- .internals$checks$check_positive
+check_tf_version <- .internals$checks$check_tf_version
 check_unit <- .internals$checks$check_unit
+
+create_progress_bar <- .internals$inference$progress_bar$create_progress_bar
+
 has_representation <- .internals$greta_arrays$has_representation
 representation <- .internals$greta_arrays$representation
+
 tf_lchoose <- .internals$tensors$tf_lchoose
 tf_lbeta <- .internals$tensors$tf_lbeta
+
+distrib <- .internals$nodes$constructors$distrib
 distribution_node <- .internals$nodes$node_classes$distribution_node
+
 as.greta_array <- .internals$greta_arrays$as.greta_array
+
 tf_iprobit <- .internals$tensors$tf_iprobit
 fl <- .internals$utils$misc$fl
 tf_as_float <- .internals$tensors$tf_as_float
