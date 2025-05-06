2ddfdb11918c0b75779f346d163e8b6eec092677
njtierney <nicholas.tierney@gmail.com>
2025-05-06 16:23:39 +1000

    misc

--- R/zero_inflated_negative_binomial.R
+++ R/zero_inflated_negative_binomial.R
@@ -21,54 +21,24 @@ zero_inflated_negative_binomial_distribution <- R6::R6Class(
   "zero_inflated_negative_binomial_distribution",
   inherit = distribution_node,
   public = list(
-    initialize = function(size, prob, pi, dim) {
+    initialize = function(size, prob, dim) {
       size <- as.greta_array(size)
       prob <- as.greta_array(prob)
-      pi <- as.greta_array(pi)
-      # add the nodes as children and parameters
-      dim <- check_dims(size, prob, pi, target_dim = dim)
-      super$initialize("zero_inflated_negative_binomial", dim, discrete = TRUE)
+      
+      # add the nodes as parents and parameters
+      dim <- check_dims(size, prob, target_dim = dim)
+      super$initialize("negative_binomial", dim, discrete = TRUE)
       self$add_parameter(size, "size")
       self$add_parameter(prob, "prob")
-      self$add_parameter(pi, "pi")
     },
+    
+    # nolint start
     tf_distrib = function(parameters, dag) {
-      size <- parameters$size
-      p <- parameters$prob # probability of success
-      pi_var <- parameters$pi
-      q <- fl(1) - parameters$prob
-      log_prob <- function(x) {
-        tf$math$log(
-          (pi_var * (fl(1) - tf$math$sign(tf$math$abs(x))) + 
-             tf$math$exp(
-               tf$math$log1p(-pi_var) + 
-                 tf_lchoose(x+size-fl(1), x) + 
-                 size * tf$math$log(p) + 
-                 x * tf$math$log1p(-p)
-             )
-          )
-        )
-      }
-      
-      sample <- function(seed) {
-        binom <- tfp$distributions$Binomial(total_count = 1, probs = pi_var)
-        negbin <-
-          tfp$distributions$NegativeBinomial(total_count = size, probs = q) # change of proba / parametrisation in 'stats'
-        
-        zi <- binom$sample(seed = seed)
-        lbd <- negbin$sample(seed = seed)
-        
-        (fl(1) - zi) * lbd
-      }
-      
-      list(
-        log_prob = log_prob,
-        sample = sample,
-        cdf = NULL,
-        log_cdf = NULL
+      tfp$distributions$NegativeBinomial(
+        total_count = parameters$size,
+        probs = fl(1) - parameters$prob
       )
-    },
-    tf_cdf_function = NULL,
-    tf_log_cdf_function = NULL
+    }
+    # nolint end
   )
 )
